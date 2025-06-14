80a6f8123c550aaa1c1704df07e1675d71fe71b4
Douglas Smith <smithda@gmail.com>
2025-06-13 22:10:43 -0500

    Switching from fillable to guarded

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,33 +16,17 @@ class User extends Authenticatable
 
     const UPDATED_AT = 'last_updated';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'first',
-        'last',
-        'email',
-        'password',
+    protected $guarded = [
+        'id',
+        'date_created',
+        'last_updated',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
