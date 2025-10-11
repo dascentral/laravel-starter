391f41673c7ea5a4fe1c09fc860409a288511241
Douglas Smith <smithda@gmail.com>
2025-10-11 09:06:47 -0400

    Re-add 2FA

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     const CREATED_AT = 'date_created';
 
@@ -24,6 +25,8 @@ class User extends Authenticatable
 
     protected $hidden = [
         'password',
+        'two_factor_secret',
+        'two_factory_recovery_codes',
         'remember_token',
     ];
 
